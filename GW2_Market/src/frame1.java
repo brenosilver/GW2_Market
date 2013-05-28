a009a28d53e18bdf91db517a7d0de2ade8a5b3a6
brenosilver <brenosilver@brenosilver-PC>
2013-05-28 14:49:42 -0400

    New Layout

--- GW2_Market/src/frame1.java
+++ GW2_Market/src/frame1.java
@@ -1,55 +1,75 @@
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 public class frame1 extends JFrame implements ActionListener {
 
 	JPanel pane = new JPanel();
-	FlowLayout layout = new FlowLayout(5,10,10);
-	
+	//FlowLayout layout = new FlowLayout(5,10,10);
+	GridLayout layout = new GridLayout();
 	
 	
 	JButton pressme = new JButton("Run Crawler");
-	JTextField bagNumber = new JTextField("", 5);
-	JLabel lab = new JLabel("Bag Number");
+	JRadioButton opt1 = new JRadioButton("Supply bag", true);
+	JRadioButton opt2 = new JRadioButton("Stolen Supplies bag", false);
 	static TextArea text = new TextArea("", 10, 70);
+	
+	ButtonGroup group = new ButtonGroup();
 	
 	String bagText;
 	
 	frame1(){
 		super("Guild Wars 2 Bag Market"); setBounds(100,200,800,250);
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    Container con = this.getContentPane(); // inherit main frame
+	   // Container con = this.getContentPane(); // inherit main frame
 	    setLayout(layout);
+	    super.setResizable(false);
 	    
-	    con.add(pane);    // JPanel containers default to FlowLayout
+	    group.add(opt1);
+	    group.add(opt2);
+		
+	   // con.add(pane);    // JPanel containers default to FlowLayout
 	    pressme.addActionListener(this);
 	    pressme.setMnemonic('P'); // associate hotkey to button
 	    
-	    bagNumber.addActionListener(this);
-	    pane.add(lab);
-	    pane.add(bagNumber);
-	    pane.add(pressme);
-	    pane.add(text);
+	    super.getContentPane().add(opt1);
+	    super.getContentPane().add(opt2);
+	    super.getContentPane().add(pressme);
+	    super.getContentPane().add(text);
+	    
+	    opt1.addActionListener(this);
+	    opt2.addActionListener(this);
+
 	    pressme.requestFocus();
+	    
 	    setVisible(true); // make frame visible
 	    
 	}
 	
+
 	public void actionPerformed(ActionEvent event){
 		Object source = event.getSource();
 		if (source == pressme){
 			text.replaceRange("", 0, 2000);
 			BasicCrawlController crawl = new BasicCrawlController();
-			String bagText = bagNumber.getText();
+			// Options
+			String bagText = "1";
+			if (opt1.isSelected())
+				bagText = "1";
+			else if (opt2.isSelected())
+				bagText = "2";
 
 			try {
 				crawl.startThis("C:\\sitecrawl", "1", bagText);
